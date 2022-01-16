4a463d7ba050c57c8c28a022430f9205915f49d7
UnaiManzanoPerez <79275029+UnaiManzanoPerez@users.noreply.github.com>
2022-01-16 01:17:33 +0100

    Push 16/01/2022 1:14AM

---
+++ src/app/services/cards.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class CardService{
+    cards=this.getCardsAjax();
+
+    constructor(private conexHttp:HttpClient){}
+
+    getCardsAjax():Observable<any>{
+        let url = "http://localhost:80/Portfolio/CardGame/php/cards.php";
+        return this.conexHttp.get(url,
+            {headers: new HttpHeaders({'Content-Type':'application/json'})}
+            );
+    }
+}
---
+++ src/database/querys.sql
@@ -0,0 +1,14 @@
+use MistbornCardGame;
+
+create table Card(
+    name varchar(30) PRIMARY KEY NOT NULL,
+    race varchar(30),
+    cardClass varchar(30),
+    type varchar(30),
+    rarity varchar(30),
+    image varchar(150),
+    health int,
+    damage int,
+    mana int,
+    abilities text
+)
